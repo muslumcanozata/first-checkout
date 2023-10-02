d46534be5faf0c0f4f2937388fcd7ac3ed073474
mco <muslumcanozata@gmail.com>
2023-10-02 23:27:55 +0300

    initial commit

---
+++ src/main/java/com/bootcamp/firstcheckout/FirstCheckoutApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.firstcheckout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstCheckoutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstCheckoutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bootcamp/firstcheckout/FirstCheckoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.firstcheckout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstCheckoutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
